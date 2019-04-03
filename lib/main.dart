fa87e18314739f9564da1fc671f3eec7ef0ff53a
lobochimuelo <lobochimuelo96@gmail.com>
2019-04-02 23:34:16 -0400

    modificando main

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,43 @@
 import 'package:flutter/material.dart';
+void main()=> runApp(MyApp());
 
-void main(){
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue[100],
-          title: Text(
-            "CRONO",
-
-            style: TextStyle(color: Colors.black45),
-          ),
-          centerTitle: true,
-          elevation: 0.0,
-          ),          
-          body: CardRoom(),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: null,
-          ),
-          bottomNavigationBar: BottomAppBar(
-            color: Colors.yellow,
-            child: Container(height: 50.0,),
-          ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        ),
-      )
-  );
-
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CRONO',
+      home: crono(),
+    );
+  }
 }
 
-class CardRoom extends StatelessWidget {
+class crono extends StatefulWidget {
+  @override
+  _cronoState createState() => _cronoState();
+}
+
+class _cronoState extends State<crono> {
+  @override
   Widget build(BuildContext context) {
-
-    
-
-    final boardTitle = Container(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[100],
+        title: Text('CRONO',
+        style: TextStyle(color: Colors.black45),
+      ),
+       centerTitle: true,
+       elevation: 0.0,
+      ),
+      body: Container(decoration: BoxDecoration(color: Colors.blue[100]),
+         child:Stack(
+        
+        children: <Widget>[
+          Container(
        padding: EdgeInsets.only(left: 10.0, bottom: 10.0,right: 15.0),        
         child: Align(
           alignment: Alignment.topLeft,
           child:  Text(
-            "Marzo",
+            "March",
             textDirection: TextDirection.ltr,
             style: TextStyle(
                 color: Colors.blue[400],
@@ -49,10 +46,10 @@ class CardRoom extends StatelessWidget {
             ),
           ),
         )
-    );
-    
-    final boardsubTitle = Container(
-          padding: EdgeInsets.only(left: 10.0,top: 30.0, bottom: 10.0,right: 15.0),        child: Align(
+    ),
+    Container(
+          padding: EdgeInsets.only(left: 10.0,top: 30.0, bottom: 10.0,right: 15.0),        
+          child: Align(
           alignment: Alignment.topLeft,
           child:  Text(
             "Tus Actividades (horas)",
@@ -64,19 +61,22 @@ class CardRoom extends StatelessWidget {
             ),
           ),
         )
-    );
-    
-     
-    return Container(
-      color: Colors.blue[100],
-      child: Stack(
-        children: <Widget>[
-          boardTitle,
-          boardsubTitle,
-          
-      
+    ),
         ],
-    )
+      )
+      ),
+       floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: null,
+          ),
+          bottomNavigationBar: BottomAppBar(
+            color: Colors.yellow,
+            child: Container(height: 50.0,),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+     
     );
   }
+  
 }
+
