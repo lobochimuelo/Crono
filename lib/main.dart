e5a643ca48575f475c49d496dcfbe868df3f61b7
lobochimuelo <lobochimuelo96@gmail.com>
2019-04-06 00:53:39 -0400

    fixing method addItem()

--- lib/main.dart
+++ lib/main.dart
@@ -102,6 +102,8 @@ class _cronoState extends State<crono> {
                              setState(() {
                                
                              });
+                             Navigator.pop(context);
+                             
                              }
    
                             ),
